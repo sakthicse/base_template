305510fa9804e810abb506c0bb0ab6c7d2f7adac
sakthicse <sakthivelr321@gmail.com>
2019-09-24 10:40:06 +0000

    save attendance and get

--- hostel_system_api/urls.py
+++ hostel_system_api/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import path,include
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework import routers
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', obtain_jwt_token),
+    path('attendance-api/', include('attendance.urls')),
 ]
